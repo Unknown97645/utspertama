4f9648ad1b930c9a1d9e5827004a5adbd7b35c54
Unknown 97645 <gadingivanda77@gmail.com>
2023-05-11 17:47:43 +0700

    UTS

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import pengguna.*;
+
 class Main {
     public static void main(String[] args) {
-    // test
+    
+        Mhs mhs = new Mhs("ini_username", "ini_password");
+
+        mhs.setNama("Gading Ali Ivanda");
+        mhs.setNim("G.111.20.0054");
+        mhs.setSmt(6);
+
+        System.out.println("Nama Mhs: " + mhs.getNama());
+        System.out.println("Nim Mhs: " + mhs.getNim());
+        System.out.println("Smt Mhs: " + mhs.getSmt());
+        System.out.println("Username: " + mhs.getUsername());
+        System.out.println("Password: " + mhs.getPassword());
     }
 }
